9a5c08f5d9cd9ecd600f0b817dcd50536e5acdc0
gastonrodrig04 <202111913@urp.edu.pe>
2024-07-21 15:24:43 -0500

    prueba

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
+import { InputComponent } from '../../shared/components/UI/input/input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [InputComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  searchTerm: string = '';
 }

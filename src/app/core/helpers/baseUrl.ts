f43375496e8e8d32825b2c85f76eebf4a712e188
gastonrodrig04 <202111913@urp.edu.pe>
2024-09-20 22:54:51 -0500

    production 2

--- src/app/core/helpers/baseUrl.ts
+++ src/app/core/helpers/baseUrl.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'https://vn-back.vercel.app/'
+export const baseUrl = 'https://vn-back.vercel.app'
 
 // 'https://backend-virgen-natividad.vercel.app'
 // 'http://localhost:3000' 

5cbfac05a0d0b6ea2080fc2376e538ef89471ccc
Gaston <gaston.rodriguez@icloud.com>
2024-10-10 16:43:31 -0500

    prueba

--- src/app/core/helpers/baseUrl.ts
+++ src/app/core/helpers/baseUrl.ts
@@ -1,4 +1,4 @@
-export const baseUrl = 'http://localhost:3000'
+export const baseUrl = 'https://vnback-5evigs57a-gastonrodrigs-projects.vercel.app/'
 
 // 'https://backend-virgen-natividad.vercel.app'
 // 'http://localhost:3000' 

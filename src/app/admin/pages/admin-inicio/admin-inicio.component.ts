52122c4dd64f2ae038b2515576af3d37cee15c62
gabsss1 <gventuram2015@gmail.com>
2024-11-22 15:39:06 -0500

    dashboard admin avance

--- src/app/admin/pages/admin-inicio/admin-inicio.component.ts
+++ src/app/admin/pages/admin-inicio/admin-inicio.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { VacanteService } from '../../../core/services/vacante.service';
 
 @Component({
   selector: 'app-admin-inicio',
@@ -7,6 +9,30 @@ import { Component } from '@angular/core';
   templateUrl: './admin-inicio.component.html',
   styleUrl: './admin-inicio.component.css'
 })
-export class AdminInicioComponent {
+export class AdminInicioComponent implements OnInit{
+  vacantesPorAnio: number = 0;
+  
+  constructor(
+    private router: Router,
+    private vacanteService: VacanteService
+  ) {}
 
+  ngOnInit(): void {
+    this.obtenerVacantesPorAño();
+  }
+
+  goToVacantes() {
+    this.router.navigate(['/admin/gestionar-vacantes']);
+  }
+
+  obtenerVacantesPorAño(): void {
+    this.vacanteService.contarVacantesPorAño().subscribe(
+      (vacantes) => {
+        this.vacantesPorAnio = vacantes;
+      },
+      (error) => {
+        console.error('Error al obtener las vacantes', error);
+      }
+    );
+  }
 }
